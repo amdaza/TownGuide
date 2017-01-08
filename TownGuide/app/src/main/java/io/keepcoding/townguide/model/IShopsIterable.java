25d887d983e90cf165af8e13529f3abda387f170
amdaza <amdazacastillo@gmail.com>
2017-01-08 19:56:22 +0100

    public IShopsIterable variables

--- TownGuide/app/src/main/java/io/keepcoding/townguide/model/IShopsIterable.java
+++ TownGuide/app/src/main/java/io/keepcoding/townguide/model/IShopsIterable.java
@@ -6,7 +6,7 @@ import java.util.List;
  * Created by Alicia on 13/12/2016.
  */
 public interface IShopsIterable {
-    long size();
-    Shop get(long index);
-    List<Shop> allShops;
+    public long size();
+    public Shop get(long index);
+    public List<Shop> allShops();
 }

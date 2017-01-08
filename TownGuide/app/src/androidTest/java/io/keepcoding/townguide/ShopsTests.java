6f6d759c70f0e3bce1681b4bddc15331ede4cf25
amdaza <amdazacastillo@gmail.com>
2017-01-08 20:11:16 +0100

    Tests for Shops

---
+++ TownGuide/app/src/androidTest/java/io/keepcoding/townguide/ShopsTests.java
@@ -0,0 +1,45 @@
+package io.keepcoding.townguide;
+
+import android.support.annotation.NonNull;
+import android.test.AndroidTestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.keepcoding.townguide.model.Shop;
+import io.keepcoding.townguide.model.Shops;
+
+/**
+ * Created by Alicia on 08/01/2017.
+ */
+
+public class ShopsTests extends AndroidTestCase {
+
+    public void testCreatingAShopsWithNullListReturnsNonNullShops() {
+        Shops sut = Shops.build(null);
+        assertNotNull(sut);
+        assertNotNull(sut.allShops());
+    }
+
+    public void testCreatingAShopsWithAListReturnsNonNullShops() {
+        List<Shop> data = getShops();
+        Shops sut = Shops.build(data);
+
+        assertNotNull(sut);
+        assertNotNull(sut.allShops());
+
+        assertEquals(sut.allShops(), data);
+        assertEquals(sut.allShops().size(), data.size());
+    }
+
+    @NonNull
+    private List<Shop> getShops() {
+        List<Shop> data = new ArrayList<>();
+        data.add(new Shop(1, "1").setAddress("AD 1"));
+        data.add(new Shop(2, "2").setAddress("AD 2"));
+        data.add(new Shop(3, "3").setAddress("AD 3"));
+        return data;
+    }
+
+
+}
